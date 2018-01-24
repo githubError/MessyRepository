e82501407625dfb5fbe37d4b3a3c1352c8f9e3e5
cpf9401@163.com <cpf9401@163.com>
2018-01-24 18:10:45 +0800

    commit

--- Python/Python3_WebApp/www/static/app.py
+++ Python/Python3_WebApp/www/static/app.py
@@ -4,9 +4,14 @@ from datetime import datetime
 
 from aiohttp import web
 
+from www.static import orm
+from www.static.models import User, Blog, Comment
+
 async def index(request):
     logging.info('--->')
     text = '<h1>Hello World! %s</h1>' % request.match_info['test']
+
+
     return web.Response(body=text.encode('utf-8'), headers={'content-type':'text/html'})
 
 async def init(loop):
@@ -16,7 +21,32 @@ async def init(loop):
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     return srv
 
+
+# 测试数据库插入
+async def testSQL(name):
+    logging.info('+++++++++++++++++++++')
+    await orm.create_pool(user='root', password='root',database='webblog')
+
+    user = User(name = name, email = 'test@email.com', passwd='1234567890', image='about:blank')
+
+    await user.save()
+
+    logging.info('--------------------')
+
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 logging.info('服务器已启动 http://127.0.0.1:9000')
+
+
+co = testSQL('test')
+myfuture1 = asyncio.ensure_future(testSQL('test'))
+loop.run_until_complete(myfuture1)
+
+
 loop.run_forever()
+
+
+
+
+
