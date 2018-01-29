8c0ac43421eb157b73261d497a9c2eda09a6b642
cpf9401@163.com <cpf9401@163.com>
2018-01-29 15:17:21 +0800

    完成初学者教程

--- Django/mysite/polls/urls.py
+++ Django/mysite/polls/urls.py
@@ -3,15 +3,29 @@
 from django.urls import path
 
 from  . import  views
+from  . import  AmendViews
 
 app_name = 'polls'
 
+
+
+# urlpatterns = [
+#     path('', views.index, name = 'index'),
+#
+#     path('<int:question_id>/', views.detail, name='detail'),
+#
+#     path('<int:question_id>/results/', views.results, name='results'),
+#
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
+
+# 使用通用视图
+
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', AmendViews.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', AmendViews.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', AmendViews.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/',AmendViews.vote, name='vote')
 
-    path('<int:question_id>/', views.detail, name='detail'),
-
-    path('<int:question_id>/results/', views.results, name='results'),
-
-    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
